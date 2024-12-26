089c81629a8961ea974cd150390619375adbd03e
msiva06 <m.sivasubramanian06@gmail.com>
2024-12-25 20:41:12 -0600

    Created PasswordEncoder Bean,UserDetailsService Bean,and included index landing page after successful login

--- login-registration/src/main/java/com/community/library/security/configuration/SecurityConfig.java
+++ login-registration/src/main/java/com/community/library/security/configuration/SecurityConfig.java
@@ -1,17 +1,55 @@
 package com.community.library.security.configuration;
 
+import com.community.library.security.service.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 @Configuration
+@AllArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private AppUserService appUserService;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return appUserService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(appUserService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
-        return  http.formLogin(form -> form.loginPage("/login").permitAll())
+        return  http.formLogin(form -> {
+                    form.loginPage("/login").permitAll();
+                    form.defaultSuccessUrl("/index");
+                })
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(register ->
                 {
                     register.requestMatchers("/signup","/css/**","/js/**").permitAll();
