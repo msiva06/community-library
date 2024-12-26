93918a8e527ff77b2f0663ee7e1be21b230fc2cc
msiva06 <m.sivasubramanian06@gmail.com>
2024-12-25 20:47:39 -0600

    Created AppUserRepository for performing db operations

---
+++ login-registration/src/main/java/com/community/library/security/repository/AppUserRepository.java
@@ -0,0 +1,12 @@
+package com.community.library.security.repository;
+
+import com.community.library.security.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser,Long> {
+    Optional<AppUser> findByUsername(String username);
+}
